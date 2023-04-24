8201a779fe246854f00e12204936a92d4e20efdf
Parthiban Rajasekaran <parthibanrajasekaran@GBMB10258.local>
2023-04-24 14:31:44 +0100

    chore: remove 'only' keyword from tests

--- e2e/paybills.spec.ts
+++ e2e/paybills.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test.describe.only("Navigate to web app security website", async () => {
+test.describe("Navigate to web app security website", async () => {
   test.beforeEach(async ({ page }) => {
     await page.goto("http://zero.webappsecurity.com");
   });
