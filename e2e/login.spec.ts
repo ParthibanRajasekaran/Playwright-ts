243e6e07d0c6e1fe5427d5a03d2d4998ee1b1f09
ParthibanRajasekaran <rajasekaran.parthiban7@gmail.com>
2023-06-06 10:21:59 +0100

    chore: refactor tests based on page classes

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -1,43 +1,43 @@
 import { test, expect } from "@playwright/test";
+import { LoginPage } from "../page-objects/LoginPage";
+import { TransferFundsPage } from "../page-objects/TransferFundsPage";
+import { HomePage } from "../page-objects/HomePage";
 
 test.describe("Navigate to web app security website", async () => {
+
+  let homePage : HomePage
+  let loginPage : LoginPage
+  let transferFundsPage : TransferFundsPage
+
   test.beforeEach(async ({ page }) => {
-    await page.goto("http://zero.webappsecurity.com");
+    homePage = new HomePage(page);
+    loginPage = new LoginPage(page); 
+    transferFundsPage = new TransferFundsPage(page);
+
+    await homePage.visit();
+    //await page.goto("http://zero.webappsecurity.com");
   });
 
   test("Invalid login", async ({ page }) => {
-    const login_btn = await page.locator("#signin_button");
-    login_btn.click();
+    homePage.signInBtn.click();
 
-    const username_txt = await page.locator("#user_login");
-    const password_txt = await page.locator("#user_password");
-    const signin_btn = await page.locator("input[name='submit']");
-    const error_msg = await page.locator(".alert-error");
+    await loginPage.usernameInput.fill("dummy-user");
+    await loginPage.passwordInput.type("dummy-password");
+    await loginPage.submitButton.click();
 
-    await username_txt.fill("dummy-user");
-    await password_txt.type("dummy-password");
-    await signin_btn.click();
-
-    await expect(error_msg).toBeVisible;
+    await expect(loginPage.errorMessage).toBeVisible;
   });
 
   test("Valid login", async ({ page }) => {
     const login_btn = await page.locator("#signin_button");
     login_btn.click();
 
-    const username_txt = await page.locator("#user_login");
-    const password_txt = await page.locator("#user_password");
-    const signin_btn = await page.locator("input[name='submit']");
-    const error_msg = await page.locator(".alert-error");
+    await loginPage.usernameInput.fill("username");
+    await loginPage.passwordInput.type("password");
+    await loginPage.submitButton.click();
+    await expect(loginPage.errorMessage).not.toBeVisible;
 
-    await username_txt.fill("username");
-    await password_txt.type("password");
-    await signin_btn.click();
-    await expect(error_msg).not.toBeVisible;
-
-    await page.goto("http://zero.webappsecurity.com/bank/transfer-funds.html");
-
-    const accountSummaryTab = await page.locator("#account_summary_tab");
-    await expect(accountSummaryTab).toBeVisible();
+    await transferFundsPage.visit();
+    await expect(transferFundsPage.accountSummaryTab).toBeVisible();
   });
 });
