f01749e3ddd62a0f6b056126f59ef98ac9a35d54
ParthibanRajasekaran <rajasekaran.parthiban7@gmail.com>
2023-06-16 16:03:40 +0100

    chore: adding sample viewport test

---
+++ tests/e2e/device.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect, devices } from '@playwright/test';
+
+test.use({
+  ...devices['iPhone 13'],
+});
+
+test.describe.only('Navigate to web app security website', async () => {
+  test('test', async ({ page }) => {
+    await page.goto('https://www.google.com/?gws_rd=ssl');
+    await page.click('button:has-text("Read more")');
+    await page.click('button:has-text("Read more")');
+    await page.click('button:has-text("Reject all")');
+    await page.goto(
+      'https://consent.yahoo.com/v2/collectConsent?sessionId=3_cc-session_8052d1c8-2efd-4a45-b51d-7f062db7b4e1'
+    );
+    await page.click('button:has-text("Reject all")');
+  });
+});
